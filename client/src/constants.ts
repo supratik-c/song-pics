f6a928caa3c28f78ffc9367c35df8e87da434284
supratik-c <scdipto@gmail.com>
2026-07-11 16:27:27 +0100

    index rm

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,8 +1,8 @@
 export const MAX_ATTEMPTS = 6;
 export const MAX_ANSWER_LENGTH = 64;
-	const today = new Date();
-	const year = today.getFullYear();
-	const month = String(today.getMonth() + 1).padStart(2, '0');
-	const day = String(today.getDate()).padStart(2, '0');
+const today = new Date();
+const year = today.getFullYear();
+const month = String(today.getMonth() + 1).padStart(2, '0');
+const day = String(today.getDate()).padStart(2, '0');
 export const PUZZLE_DIRECTORY = `/content/puzzles/`;
 export const TODAY_PUZZLE_PATH = `/content/puzzles/${year}-${month}-${day}.json`;
